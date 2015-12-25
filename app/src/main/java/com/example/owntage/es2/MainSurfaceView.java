b885989a6ca512ad24b42ff5beb25d036565c814
Owntage <owntage@yandex.ru>
2015-12-25 06:46:11 -0800

    thread safety improved

--- app/src/main/java/com/example/owntage/es2/MainSurfaceView.java
+++ app/src/main/java/com/example/owntage/es2/MainSurfaceView.java
@@ -16,7 +16,6 @@ public class MainSurfaceView extends GLSurfaceView {
     public MainSurfaceView(Context context, MainRenderer renderer) {
         super(context);
         this.renderer = renderer;
-        //setRenderMode(RENDERMODE_CONTINUOUSLY);
     }
 
     @Override
@@ -27,37 +26,56 @@ public class MainSurfaceView extends GLSurfaceView {
         float y;
         int touchID;
 
+        class OnClickRunnable implements Runnable {
+            float x;
+            float y;
+            int touchID;
+            boolean onDown;
+            public OnClickRunnable(float x, float y, int touchID, boolean onDown) {
+                this.x = x;
+                this.y = y;
+                this.touchID = touchID;
+                this.onDown = onDown;
+            }
+            public void run() {
+                if(onDown) {
+                    renderer.onDown(x, y, touchID);
+                } else {
+                    renderer.onUp(x, y, touchID);
+                }
+            }
+        }
 
         switch(event.getActionMasked()) {
             case MotionEvent.ACTION_UP:
                 x = event.getX();
                 y = event.getY();
                 touchID = event.getPointerId(event.getActionIndex());
-                renderer.onUp(x, y, touchID);
+                queueEvent(new OnClickRunnable(x, y, touchID, false));
                 break;
             case MotionEvent.ACTION_POINTER_UP:
                 x = event.getX(event.getPointerId(event.getActionIndex()));
                 y = event.getY(event.getPointerId(event.getActionIndex()));
                 touchID = event.getPointerId(event.getActionIndex());
-                renderer.onUp(x, y, touchID);
+                queueEvent(new OnClickRunnable(x, y, touchID, false));
                 break;
             case MotionEvent.ACTION_MOVE:
-
                 //renderer.onMove(x, y, touchID);
-
                 break;
             case MotionEvent.ACTION_DOWN:
 
                 x = event.getX();
                 y = event.getY();
                 touchID = event.getPointerId(event.getActionIndex());
-                renderer.onDown(x, y, touchID);
+
+                queueEvent(new OnClickRunnable(x, y, touchID, true));
                 break;
             case MotionEvent.ACTION_POINTER_DOWN:
                 x = event.getX(event.getPointerId(event.getActionIndex()));
                 y = event.getY(event.getPointerId(event.getActionIndex()));
                 touchID = event.getPointerId(event.getActionIndex());
-                renderer.onDown(x, y, touchID);
+
+                queueEvent(new OnClickRunnable(x, y, touchID, true));
                 break;
         }
 
